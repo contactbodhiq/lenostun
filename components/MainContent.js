2b08b57197673b6422b8175067359b3c1296eeab
Varsha <contactbodhiq@gmail.com>
2025-07-30 12:55:13 +0530

    Initial commit

---
+++ components/MainContent.js
@@ -0,0 +1,1 @@
+export default function MainContent() { return <main>Main</main>; }
---
+++ pages/index.js
@@ -0,0 +1,1 @@
+import Header from '@/components/Header'; import Footer from '@/components/Footer'; import MainContent from '@/components/MainContent'; export default function Home() { return (<><Header /><MainContent /><Footer /></>); }
